f4c62363e891c8dd3b9ec074e21a3b5bb0c82c30
Johnny Vestergaard <jkv@unixcluster.dk>
2016-04-17 14:37:41 +0200

    disable ssh and telnet
    #15

--- heralding/capabilities/__init__.py
+++ heralding/capabilities/__init__.py
@@ -4,8 +4,9 @@ import os
 # Detect all modules
 for fullname in glob.glob(os.path.dirname(__file__) + "/*.py"):
     name = os.path.basename(fullname)
-    # __init__ and handlerbase are not capabilities, so ignore them 
-    if name[:-3] == "__init__" or name[:-3] == "handlerbase":
+    # __init__ and handlerbase are not capabilities, so ignore them
+    # also ignore ssh and telnet until telnetsrvlib har python3 support
+    if name[:-3] == "__init__" or name[:-3] == "handlerbase" or name[:-3] == "ssh" or name[:-3] == "telnet":
         pass
     else:
         __import__("heralding.capabilities." + name[:-3])
